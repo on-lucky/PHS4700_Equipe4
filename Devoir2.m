70588a3d6935acaf4e6b7eac6444918bca2ec88e
Jérémie Jasmin <jeremiejasmin1@hotmail.com>
2017-10-02 16:35:44 -0400

    Collisions + changements mineurs

--- Devoir2.m
+++ Devoir2.m
@@ -1,6 +1,10 @@
 function [coup, tf, rbf, vbf] = Devoir2(option, rbi, vbi, wbi)
-    coup = 1;
-    tf = 10;
-    rbf = [1; 1; 1];
-    vbf = [2; 2; 2];
+    DeltaT = 0.01;
+    if (option == Variables.option1)
+        [rbf, vbf, tf, coup] = Option1.option1(rbi, vbi, wbi, DeltaT);
+    elseif (option == Variables.option2)
+        [rbf, vbf, tf, coup] = Option1.option1(rbi, vbi, wbi, DeltaT);
+    elseif (option == Variables.option3)
+        [rbf, vbf, tf, coup] = Option1.option1(rbi, vbi, wbi, DeltaT);
+    end
 end
---
+++ Variables.m
@@ -0,0 +1,26 @@
+classdef Variables
+    properties (Constant)
+        coup0 = 0; %coup réussi
+        coup1 = 1; %coup du mauvais côté de la table
+        coup2 = 2; %coup dans le filet
+        coup3 = 3; %coup au sol
+        coupIntdetermine = -1; %coup en progression
+        
+        option1 = 1;
+        option2 = 2;
+        option3 = 3;
+        
+        rb = 0.0199;
+        
+        hTable = 0.76;
+        longTable = 2.74;
+        largTable = 1.525;
+        
+        hFilet = 0.1525;
+        largFilet = 1.83;
+        debordementFilet = 0.1525;
+    end
+    
+    methods (Static)
+    end
+end
