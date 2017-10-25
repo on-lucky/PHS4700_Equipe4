c3ee498fe21ea3813441328d8877f639146c5d8e
CarolineSJ <carolinesabourin-jean@hotmail.com>
2017-10-25 14:44:16 -0400

    Ajout de la méthode pour trouver la position sans la collision et pour faire les graphes

--- Devoir3/Variables.m
+++ Devoir3/Variables.m
@@ -7,24 +7,38 @@ classdef Variables
         la = 1.82;
         ha = 1.8;
         
+        raExterne = sqrt((Variables.La/2)^2 + (Variables.la/2)^2);
+        
         %voiture B
         mb = 1010;
         Lb = 4.23;
         lb = 1.6;
         hb = 1.8;
         
+        rbExterne = sqrt((Variables.Lb/2)^2 + (Variables.lb/2)^2);
+        
         aGrav = 9.81;
         
         coefficientRes = 0.8;
         
+        erreur = 0.01;
+        vitesseMinimaleSimulation = 0.01;
+        
+        collIndetermine = -1;
+        collReussie = 0;
+        collRatee = 1;
+        collProximite = 2;
+        
+        avecFrot = true;
     end
-  methods (Static)   
-    function coFrot = coefficientFrot(vitesse)
-        if (norm(vitesse) < 50) 
-            coFrot = 0.15 * (1 - (norm(vitesse) / 100));
-        else
-            coFrot = 0.075;
-        end  
+    
+    methods (Static)   
+        function coFrot = coefficientFrot(vitesse)
+            if (norm(vitesse) < 50) 
+                coFrot = 0.15 * (1 - (norm(vitesse) / 100));
+            else
+                coFrot = 0.075;
+            end  
+        end
     end
-  end
 end
