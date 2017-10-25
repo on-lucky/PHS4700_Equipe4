cb34af7332650f3b58afde9707c1627cba1b1fbf
CarolineSJ <carolinesabourin-jean@hotmail.com>
2017-10-25 13:34:01 -0400

    Main + fonct dev3

---
+++ Devoir3/Devoir3.m
@@ -0,0 +1,3 @@
+function [Coll, tf, raf, vaf, rbf, vbf] = Devoir3(rai, vai, rbi, vbi, tb)
+    [Coll, tf, raf, vaf, rbf, vbf] = Outils.avantCollision(rai, vai, rbi, vbi, tb);
+end
---
+++ Devoir3/Main.m
@@ -0,0 +1,55 @@
+rai = [0; 0];
+
+% Essai 1
+
+vai_1 = [20; 0; 2];
+rbi_1 = [100; 100];
+vbi_1 = [0; -20; -1];
+tb_1 = 0;
+
+[Coll_1, tf_1, raf_1, vaf_1, rbf_1, vbf_1] = Devoir3(rai, vai_1, rbi_1, vbi_1, tb_1)
+
+% % Essai 2
+% 
+% vai_2 = [30; 0; 2];
+% rbi_2 = [100; 100];
+% vbi_2 = [0; -30; -1];
+% tb_2 = 0;
+% 
+% [Coll_2, tf_2, raf_2, vaf_2, rbf_2, vbf_2] = Devoir3(rai, vai_2, rbi_2, vbi_2, tb_2)
+%     
+% % Essai 3
+% 
+% vai_3 = [20; 0; 2];
+% rbi_3 = [100; 50];
+% vbi_3 = [0; -10; 0];
+% tb_3 = 1.6;
+% 
+% [Coll_3, tf_3, raf_3, vaf_3, rbf_3, vbf_3] = Devoir3(rai, vai_3, rbi_3, vbi_3, tb_3)
+% 
+% % Essai 4
+% 
+% vai_4 = [10; 10; 1];
+% rbi_4 = [25; 10];
+% vbi_4 = [10; 0; 0];
+% tb_4 = 0;
+% 
+% [Coll_4, tf_4, raf_4, vaf_4, rbf_4, vbf_4] = Devoir3(rai, vai_4, rbi_4, vbi_4, tb_4)
+% 
+% % Essai 5
+% 
+% vai_5 = [20; 0; 2];
+% rbi_5 = [100; 50];
+% vbi_5 = [0; -10; 0];
+% tb_5 = 0;
+% 
+% [Coll_5, tf_5, raf_5, vaf_5, rbf_5, vbf_5] = Devoir3(rai, vai_5, rbi_5, vbi_5, tb_5)
+% 
+% % Essai 6
+% 
+% vai_6 = [20; 2; 2];
+% rbi_6 = [100; 10];
+% vbi_6 = [10; 0; 5];
+% tb_6 = 1;
+% 
+% [Coll_6, tf_6, raf_6, vaf_6, rbf_6, vbf_6] = Devoir3(rai, vai_6, rbi_6, vbi_6, tb_6)
