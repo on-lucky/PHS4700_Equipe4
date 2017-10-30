22d3c28e8ea38d1d6dd85ac35acac2cfeb1cf8a7
Jérémie Jasmin <jeremiejasmin1@hotmail.com>
2017-10-30 12:01:46 -0400

    ajout du calcul des collisions dans outils
    Jeremie

--- Outils.m
+++ Outils.m
@@ -1,41 +1,56 @@
 classdef Outils
     methods (Static)
-        function genererGraphe(trajectoire, size, option) 
-            
-            %dessiner la table
-            ptsTable = [Variables.pointTable1; Variables.pointTable2; Variables.pointTable3; Variables.pointTable4; Variables.pointTable1];
-            ligneTable = plot3(ptsTable(:,1), ptsTable(:,2), ptsTable(:,3))
-            ligneTable.Color = 'blue';
-            
-            hold on
-            
-            %dessiner le filet
-            ptsFilet = [Variables.pointFilet1; Variables.pointFilet2; Variables.pointFilet3; Variables.pointFilet4; Variables.pointFilet1];
-            ligneFilet = plot3(ptsFilet(:,1), ptsFilet(:,2), ptsFilet(:,3))
-            ligneFilet.Color = 'red';
-            
-            hold on
-            
-            X = zeros(size, 1);
-            Y = zeros(size, 1);
-            Z = zeros(size, 1);
-            for i = 1:size
-                X(i) = trajectoire(i, 1);
-                Y(i) = trajectoire(i, 2);
-                Z(i) = trajectoire(i, 3);
+        function [Coll, tf, raf, vaf, rbf, vbf] = avantCollision(rai, vai, rbi, vbi, tb)
+            q0a = [vai(1) vai(2) ...
+				  rai(1) rai(2)];
+            q0b = [vbi(1) vbi(2) ...
+				  rbi(1) rbi(2)];
+            trajectoirea = [];
+            trajectoireb = [];
+            DeltaT  = Variables.erreur / Outils.vitessePlusVite([vai(1) vai(2)], [vbi(1) vbi(2)]);
+            t0 = 0;
+            coll = Variables.collIndetermine;
+            while (coll == Variables.collIndetermine)
+                t0 = t0 + DeltaT;
+                q0a = Outils.SEDRK4t0(q0a, DeltaT, Variables.avecFrot);
+                if (t0 >= tb)
+                    q0b = Outils.SEDRK4t0(q0b, DeltaT, Variables.avecFrot);
+                else
+                    q0b = Outils.SEDRK4t0(q0b, DeltaT, ~Variables.avecFrot);
+                end
+                trajectoirea = [trajectoirea; q0a(3) q0a(4)];
+                trajectoireb = [trajectoireb; q0b(3) q0b(4)];
+                
+                coll = Collisions.verifierProximite([q0a(3) q0a(4)], [q0b(3) q0b(4)]);
+                if(coll == Variables.collProximite)
+                    coll = Collisions.planDivision([q0a(3) q0a(4)], [q0b(3) q0b(4)], angleRotA, angleRotB);
+                
+                vitessePlusVite = Outils.vitessePlusVite([q0a(1) q0a(2)], [q0b(1) q0b(2)]);
+                if (vitessePlusVite < Variables.vitesseMinimaleSimulation)
+                    coll = Variables.collRatee;
+                end
+                
+                DeltaT  = Variables.erreur / vitessePlusVite;
             end
-            plot3(X, Y, Z); hold on
-            text(X(size), Y(size), Z(size), strcat('Option ', num2str(option)));
-            xlabel('x');
-            ylabel('y');
-            zlabel('z');
-            title('Essai 1');
-            % title('Essai 2');
-            % title('Essai 3');
-            % title('Essai 4');
+            tf = t0;
+            Coll = coll;
+            vaf = [q0a(1) q0a(2) vai(3)];
+            raf = [q0a(3) q0a(4)];
+            vbf = [q0b(1) q0b(2) vbi(3)];
+            rbf = [q0b(3) q0b(4)];
+            sizeTrajectoire = size(trajectoirea);
+            Outils.genererGraphe(trajectoirea, trajectoireb, sizeTrajectoire(1));
         end
         
-        function qs = SEDRK4t0(q0, DeltaT, option)
+        function v = vitessePlusVite(va, vb)
+            if (norm(va) < norm(vb))
+                v = norm(vb);
+            else
+                v = norm(va);
+            end
+        end
+        
+        function qs = SEDRK4t0(q0, DeltaT, avecFrot)
 			% Solution d'equations differentielles
 			% par methode de RK4
 			% Equation a resoudre : dq/dt = g(q ,t)
@@ -43,11 +58,12 @@ classdef Outils
 			% qs : solution [q(t0 + DeltaT)]
 			% q0 : conditions initiales [q(t0)]
 			% DeltaT : intervalle de temps
-			% g : membre de droite de ED.
-			% C'est un m-file de matlab
-			% qui retourne la valeur de g
-			% au temps choisi
-            gOpt = strcat('Outils.gOption', num2str(option));
+            if (avecFrot)
+                gOpt = 'Outils.gAvecFrot';
+            else
+                gOpt = 'Outils.gSansFrot';
+            end
+            
 			k1 = feval(gOpt, q0); % param t0 de la fonction gOpt n'est pas utilise, donc on l'a envele, mais sinon, on aurait mis t0 comme 3e param à feval
 			k2 = feval(gOpt, q0 + k1 * DeltaT/2); % param t0 de la fonction gOpt n'est pas utilise, donc on l'a envele, mais sinon, on aurait mis t0 + DeltaT/2 comme 3e param à feval
 			k3 = feval(gOpt, q0 + k2 * DeltaT/2); % param t0 de la fonction gOpt n'est pas utilise, donc on l'a envele, mais sinon, on aurait mis t0 + DeltaT/2 comme 3e param à feval
@@ -55,37 +71,61 @@ classdef Outils
 			qs = q0 + DeltaT * (k1 + 2 * k2 + 2 * k3 + k4)/6;
         end
         
-        function g = gOption1(q0)
-            % dvx(t)/dt = 0
-            % dvy(t)/dt = 0
-            % dvz(t)/dt = -9.81
-			% acceleration angulaire = [0 0 0] car w constant
-            g = [0 0 Variables.aGrav ...
-			     q0(1) q0(2) q0(3) ...
-			     0 0 0];
+        function g = gAvecFrot(q0)
+            % dv(t)/dt = -u(v) * g * v/|v|
+            
+            v = [q0(1) q0(2)];
+            acceleration = - Variables.coefficientFrot(v) * Variables.aGrav * v / norm(v);
+            
+            g = [acceleration(1) acceleration(2) ...
+			     q0(1) q0(2)];
         end
         
-        function g = gOption2(q0)
-            % dv(t)/dt = (-p*Cv*A*|v|*v)/(2*mb) + [0 0 -9.81]
-			% acceleration angulaire = [0 0 0] car w constant
-            v = [q0(1) q0(2) q0(3)];
-            acceleration = (-Variables.p) * Variables.Cv * Variables.A * norm(v) * v / (2 * Variables.mb) + [0 0 Variables.aGrav];
-            g = [acceleration(1) acceleration(2) acceleration(3) ...
-			     q0(1) q0(2) q0(3) ...
-			     0 0 0];
+        function g = gSansFrot(q0)
+            % dv(t)/dt = [0 0] car vitesse constante vu qu'il n'y a pas de
+            % force appliquée sur la voiture
+            
+            g = [0 0 ...
+			     q0(1) q0(2)];
         end
         
-        function g = gOption3(q0)
-            % dv(t)/dt = 4*pi*Cm*p*Rb^3*(w x v)/mb + (-p*Cv*A*|v|*v)/(2*mb) + [0 0 -9.81]
-			% acceleration angulaire = [0 0 0] car w constant
-            v = [q0(1) q0(2) q0(3)];
-            w = [q0(7) q0(8) q0(9)];
-            acceleration = 4 * pi * Variables.Cm * Variables.p * Variables.rb^3 * (cross(w, v))/Variables.mb ...
-                           + (-Variables.p) * Variables.Cv * Variables.A * norm(v) * v / (2 * Variables.mb)...
-                           + [0 0 Variables.aGrav];
-            g = [acceleration(1) acceleration(2) acceleration(3) ...
-			     q0(1) q0(2) q0(3) ...
-			     0 0 0];
+        function genererGraphe(trajectoirea, trajectoireb, size) 
+            
+            %dessiner la table
+%             ptsTable = [Variables.pointTable1; Variables.pointTable2; Variables.pointTable3; Variables.pointTable4; Variables.pointTable1];
+%             ligneTable = plot3(ptsTable(:,1), ptsTable(:,2), ptsTable(:,3))
+%             ligneTable.Color = 'blue';
+%             
+%             hold on
+            
+            X = zeros(size, 1);
+            Y = zeros(size, 1);
+            for i = 1:size
+                X(i) = trajectoirea(i, 1);
+                Y(i) = trajectoirea(i, 2);
+            end
+            plot(X, Y);
+            text(X(size), Y(size), 'Voiture a');
+            
+            hold on
+            
+            X = zeros(size, 1);
+            Y = zeros(size, 1);
+            for i = 1:size
+                X(i) = trajectoireb(i, 1);
+                Y(i) = trajectoireb(i, 2);
+            end
+            plot(X, Y);
+            text(X(size), Y(size), 'Voiture b');
+            
+            xlabel('x');
+            ylabel('y');
+            title('Essai 1');
+            % title('Essai 2');
+            % title('Essai 3');
+            % title('Essai 4');
+            % title('Essai 5');
+            % title('Essai 6');
         end
     end
 end
