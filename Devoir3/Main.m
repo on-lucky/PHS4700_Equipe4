25cc32dccd8f6eb1aabbaef9988bc9f45992f552
Jérémie Jasmin <jeremiejasmin1@hotmail.com>
2017-11-08 14:44:56 -0500

    Ca marche!
    -Jérémie, Yujia, JustineSurGitHub

--- Devoir3/Main.m
+++ Devoir3/Main.m
@@ -2,12 +2,12 @@ rai = [0; 0];
 
 % Essai 1
 
-vai_1 = [20; 0; 2];
-rbi_1 = [100; 100];
-vbi_1 = [0; -20; -1];
-tb_1 = 0;
-
-[Coll_1, tf_1, raf_1, vaf_1, rbf_1, vbf_1] = Devoir3(rai, vai_1, rbi_1, vbi_1, tb_1)
+% vai_1 = [20; 0; 2];
+% rbi_1 = [100; 100];
+% vbi_1 = [0; -20; -1];
+% tb_1 = 0;
+% 
+% [Coll_1, tf_1, raf_1, vaf_1, rbf_1, vbf_1] = Devoir3(rai, vai_1, rbi_1, vbi_1, tb_1)
 
 % % Essai 2
 % 
@@ -17,8 +17,8 @@ tb_1 = 0;
 % tb_2 = 0;
 % 
 % [Coll_2, tf_2, raf_2, vaf_2, rbf_2, vbf_2] = Devoir3(rai, vai_2, rbi_2, vbi_2, tb_2)
-%     
-% % Essai 3
+    
+% Essai 3
 % 
 % vai_3 = [20; 0; 2];
 % rbi_3 = [100; 50];
@@ -26,8 +26,8 @@ tb_1 = 0;
 % tb_3 = 1.6;
 % 
 % [Coll_3, tf_3, raf_3, vaf_3, rbf_3, vbf_3] = Devoir3(rai, vai_3, rbi_3, vbi_3, tb_3)
-% 
-% % Essai 4
+
+% Essai 4
 % 
 % vai_4 = [10; 10; 1];
 % rbi_4 = [25; 10];
@@ -47,9 +47,9 @@ tb_1 = 0;
 % 
 % % Essai 6
 % 
-% vai_6 = [20; 2; 2];
-% rbi_6 = [100; 10];
-% vbi_6 = [10; 0; 5];
-% tb_6 = 1;
-% 
-% [Coll_6, tf_6, raf_6, vaf_6, rbf_6, vbf_6] = Devoir3(rai, vai_6, rbi_6, vbi_6, tb_6)
+vai_6 = [20; 2; 2];
+rbi_6 = [100; 10];
+vbi_6 = [10; 0; 5];
+tb_6 = 1;
+
+[Coll_6, tf_6, raf_6, vaf_6, rbf_6, vbf_6] = Devoir3(rai, vai_6, rbi_6, vbi_6, tb_6)
